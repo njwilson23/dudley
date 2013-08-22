2bebda695fe6469d7257b13cde29e90aa0fb6a26
Nat Wilson <njwilson23@gmail.com>
2013-08-22 14:15:48 -0800

    add distutils install script

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+
+setup(name="Dudley",
+      version="0.1",
+      description="Simple USB filesystem mount utility",
+      author="Nat Wilson",
+      scripts=["dudley.py"]
+      )
+
