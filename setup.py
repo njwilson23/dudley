b697e291d701c3db1d451f4d432ee0e35c5b8b07
Nat Wilson <njwilson23@gmail.com>
2013-10-12 11:19:38 -0400

    lowercase name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 from distutils.core import setup
 
-setup(name="Dudley",
+setup(name="dudley",
       version="0.1",
       description="Simple USB filesystem mount utility",
       author="Nat Wilson",
